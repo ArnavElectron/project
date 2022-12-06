75ee4551de230fb56a12141844702937628acf87
Arnav Rao <arnavrao100@gmail.com>
2022-12-06 22:53:25 +0530

    more progress

--- frontend/landingtab.py
+++ frontend/landingtab.py
@@ -1,18 +1,31 @@
 import customtkinter
-
+import tkinter.messagebox
+import tkinter
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-
-        self.title("minimal example app")
-        self.minsize(400, 300)
-
-        self.button = customtkinter.CTkButton(master=self, command=self.button_callback)
-        self.button.pack(row=0,column=2,padx=20, pady=20)
-
-    def button_callback(self):
+        self.title("Inventory Management System")
+        self.geometry(f"{600}x{500}")
+        self.entry = customtkinter.CTkEntry(self, placeholder_text="username")
+        self.entry.place(relx=0.5,rely=0.4,anchor=tkinter.CENTER)
+        self.entry2 = customtkinter.CTkEntry(self, placeholder_text="password")
+        self.entry2.place(relx=0.5,rely=0.5,anchor=tkinter.CENTER)
+        self.button = customtkinter.CTkButton(self,
+                                 width=120,
+                                 height=32,
+                                 border_width=0,
+                                 corner_radius=8,
+                                 text="Submit",
+                                 command=self.button_event)
+        self.button.place(relx=0.7, rely=0.6, anchor=tkinter.CENTER)
+    def button_event(self):
         print("button pressed")
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+        username=self.entry.get()
+        password=self.entry2.get()
+        print("Username : ",username ,"\nPassword : ",password)
+
+app = App()
+app.mainloop()
--- tests/test_complex.py
+++ tests/test_complex.py
@@ -2,7 +2,7 @@ import tkinter
 import tkinter.messagebox
 import customtkinter
 
-customtkinter.set_appearance_mode("Light")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 
