4a9e1f84e4ea42c004a1ece1c8e33f12d8f1b887
Arnav Rao <arnavrao100@gmail.com>
2022-12-25 17:54:41 +0530

    aded main

---
+++ tests/testgetpass.py
@@ -0,0 +1,6 @@
+# import hashlib
+# hashed=hashlib.sha256(b"hello")
+# print(hashed.hexdigest())
+# print("insert into users values (\""+username+"\",\""+password+"\",\""+time.time+"\",\""+time.time+"\"")
+from datetime import datetime
+print(datetime.now())
